9488495171e93849e0bdf5a9a13f584fe81809c3
lidawei <ldwwining@163.com>
2023-01-12 16:28:20 +0800

    合并key文件

--- main.py
+++ main.py
@@ -1,88 +1,76 @@
 import datetime
 import os
 import shutil
-import time
 from functools import partial
-from tempfile import TemporaryDirectory, NamedTemporaryFile
+from tempfile import TemporaryDirectory
+
 from cryptography.fernet import Fernet
 
 
 class SecMod:
     def __init__(self):
         self.files = []
-        # upath = unicode("D:\\运维手册", 'utf-8')
-        os.chdir("D:\\a")
         for file in os.listdir():
-            # 排除“自己人”
-            if file == "file.key" or file == "加密.exe" or file == "解密.exe":
+            # 排除特定文件
+            if file == "加密.exe" or file == "解密.exe":
                 continue
             if os.path.isfile(file):
                 # 获取该路径下文件的list列表
                 self.files.append(file)
 
     def encrypt(self):
+        """
+        加密文件
+        """
         key = Fernet.generate_key()
-        with open("file.key", "wb") as thekey:
-            thekey.write(key)
+        temp_dir = TemporaryDirectory()
+        start_time = datetime.datetime.now()
         for file in self.files:
-            with open(file, "rb", buffering=0) as thefile:
-                _dir = TemporaryDirectory()
-                temp_file = NamedTemporaryFile(dir=_dir.name, mode='w+b')
-                start_time = datetime.datetime.now()
-                for chunk in chunked_file_reader(thefile):
-                    with temp_file as f:
-                        f.write(Fernet(key).encrypt(chunk))
-                shutil.copy(temp_file.name, file)
-                use_time = datetime.datetime.now() - start_time
-                print(use_time)
+            with open(file, "rb", buffering=0) as t, open(temp_dir.name + file, "ab+") as f:
+                # 文件头2位写入key的长度，之后写入key，然后通过流形式写入一个临时文件，完成后替换当前文件
+                f.write(len(key).to_bytes(2, 'big'))
+                f.write(key)
+                for chunk in chunked_file_reader(t):
+                    f.write(Fernet(key).encrypt(chunk))
+                shutil.copy(temp_dir.name + file, file)
+        use_time = datetime.datetime.now() - start_time
+        print(use_time)
 
     def decrypt(self):
+        """
+        解密文件
+        :return:
+        """
         try:
-            with open("file.key", "rb", buffering=4096) as key:
-                right_key = key.read()
-            # for file in self.files:
-            #     with open(file, "rb") as thefile:
-            #         contents = thefile.read()
-            #         start_time = datetime.datetime.now()
-            #         contents_decrypted = Fernet(right_key).decrypt(contents)
-            #         use_time = datetime.datetime.now() - start_time
-            #         print(use_time)
-            #     with open('b.avi', "ab+") as thefile:
-            #         thefile.write(contents_decrypted)
+            temp_dir = TemporaryDirectory()
+            start_time = datetime.datetime.now()
             for file in self.files:
-                start_time = datetime.datetime.now()
-                with open(file, "rb", buffering=0) as thefile:
-                    for chunk in chunked_file_reader(thefile):
-                        with open("b.avi", "ab+") as f:
-                            f.write(Fernet(right_key).decrypt(chunk))
-                use_time = datetime.datetime.now() - start_time
-                print(use_time)
+                with open(file, "rb", buffering=0) as t, open(temp_dir.name + file, "ab+") as f:
+                    # 通过文件头两位得知key长度，之后读取key并解密
+                    key_len = t.read(2)
+                    right_key = t.read(int.from_bytes(key_len,'big'))
+                    for chunk in chunked_file_reader(t, block_size=5592504):
+                        f.write(Fernet(right_key).decrypt(chunk))
+                    shutil.copy(temp_dir.name + file, file)
+            use_time = datetime.datetime.now() - start_time
+            print(use_time)
 
             print("恭喜，你的文件已成功解锁")
         except IOError:
             pass
 
-    def test(self):
-        for file in self.files:
-            start_time = datetime.datetime.now()
-            with open(file, "rb", buffering=0) as thefile:
-                for chunk in chunked_file_reader(thefile):
-                    with open('a.avi', "ab+") as t:
-                        t.write(chunk)
-            use_time = datetime.datetime.now() - start_time
-            print(use_time)
 
-
-# def chunked_file_reader(file, block_size=1398200):
-def chunked_file_reader(file, block_size=1024 * 1024):
+def chunked_file_reader(file, block_size=1024 * 1024 * 4):
     """生成器函数：分块读取文件内容，使用 iter 函数
     """
     # 首先使用 partial(fp.read, block_size) 构造一个新的无需参数的函数
     # 循环将不断返回 fp.read(block_size) 调用结果，直到其为 '' 时终止
+    # 加密段每次读取1M 解密段每次需读取1398200字节
     for chunk in iter(partial(file.read, block_size), b''):
         yield chunk
 
 
 if __name__ == '__main__':
+    os.chdir(os.getcwd())
     sm = SecMod()
     sm.encrypt()
