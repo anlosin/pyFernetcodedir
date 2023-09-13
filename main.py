cd7c0959b24b31bdbcede4f1b9fd8e92d9d1bcdc
lidawei <ldwwining@163.com>
2023-09-13 16:01:39 +0800

    合并key文件

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from securedir import SecMod
 
 if __name__ == '__main__':
     os.chdir(os.getcwd())
-    sm = SecMod()
     for file in os.listdir():
         # 排除特定文件
         if file == "加密.exe" or file == "解密.exe":
             continue
         if os.path.isfile(file):
             # 获取该路径下文件的list列表
-            sm.encrypt(file)
+            SecMod.encrypt(file)
